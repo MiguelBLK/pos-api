a402dbf5b7e8cf43a033dbdb4e9bc912bd79b942
ozmansan <alexis_sanchezv@hotmail.com>
2024-09-11 19:55:40 -0600

    [Cambios_back_oz][change] se agregan cambios de back

--- app/Http/Controllers/Api/StoreTypeController.php
+++ app/Http/Controllers/Api/StoreTypeController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\StoreTypes;
 use Illuminate\Http\Request;
+use App\Validators\StoreTypesValidator;
+use Illuminate\Http\JsonResponse;
 
 class StoreTypeController extends Controller
 {
@@ -33,5 +35,37 @@ class StoreTypeController extends Controller
         ];
 
         return response()->json( $data, 200);
+    }
+
+    public function create(Request $request)
+    {
+
+        $validator = new StoreTypesValidator($request);
+        $validationResult = $validator->validate();
+
+        if ($validationResult instanceof JsonResponse) {
+            return $validationResult;
+        }
+
+        $storeTypes = StoreTypes::create([
+            'name' => $request->name
+        ]);
+
+        if (!$storeTypes) {
+            $data = [
+                'message' => 'Error al crear tipo de tienda',
+                'status' => 500
+            ];
+
+            return response()->json($data, 500);
+        }
+
+        $data = [
+            'result' => $storeTypes,
+            'message' => 'Tipo de tienda creada con exito',
+            'status' => 200
+        ];
+
+        return response()->json($data, 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,22 @@
 <?php
 
 use App\Http\Controllers\Api\StoreTypeController;
+use App\Http\Controllers\Api\StatusesController;
+use App\Http\Controllers\Api\EmployeeController;
 use App\Http\Controllers\Api\StoreController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 
 Route::get('/store-type', [StoreTypeController::class, 'index']);
+Route::post('/store-type', [StoreTypeController::class, 'create']);
+
+Route::get('/store', [StoreController::class, 'index']);
 Route::post('/store', [StoreController::class, 'create']);
+
+Route::get('/statuses', [StatusesController::class, 'index']);
+Route::post('/statuses', [StatusesController::class, 'create']);
+
+Route::get('/employees', [EmployeeController::class, 'index']);
+Route::post('/employees', [EmployeeController::class, 'create']);
